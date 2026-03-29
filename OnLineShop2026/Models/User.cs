7b5cc9cca7afbfd8ee628f91a42595e3f67ddd37
ParzivaloC <bicherahovartur12@mail.ru>
2026-03-29 21:54:00 +0300

    Подзадача 1 Создал класс User
    Добавил поля Id , FirstName , LastName, Email , AvatarPath
    
    По умолчанию для аватара используется путь "/images/default_avatar.png" ( для условия )
    
    Добавил меттод ToString для удобного отображения инфо о пользователся
    
    Добавил картинку для аватара профиля

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,36 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        private const string DefaultAvatar = "/images/default_avatar.jpg";
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string AvatarPath { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            AvatarPath = DefaultAvatar;
+        }
+
+        public User(string firstName, string lastName, string email, string? avatarPath = null)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName ?? string.Empty;
+            LastName = lastName ?? string.Empty;
+            Email = email ?? string.Empty;
+            AvatarPath = string.IsNullOrEmpty(avatarPath) ? DefaultAvatar : avatarPath!;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+        public override string ToString()
+        {
+            return $"Пользователь: {FullName}, Email: {Email}";
+        }
+    }
+}
